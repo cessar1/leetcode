b1186697e361d84c438fa53e4fd7e8ebd704e4ed
Cesar Fernandez <chf05ster@gmail.com>
2025-07-03 22:45:37 -0300

    topKFrequentElements exercise + test

---
+++ src/__test__/topKFrequentElements.test.js
@@ -0,0 +1,10 @@
+const topKFrequentElements = require('../topKFrequentElements')
+
+test('topKFrequentElements', () => {
+  const inputArray = [1, 2, 1, 3, 1, 4, 2]
+  const count = 2
+  const expected = [1,2]
+  const result = topKFrequentElements(inputArray, count)
+
+  expect(result).toStrictEqual(expected)
+})
---
+++ src/topKFrequentElements.js
@@ -0,0 +1,23 @@
+function topKFrequentElements(nums, k) {
+  const count = {}
+  const freq = Array.from({ length: nums.length + 1 }, () => [])
+
+  for (const n of nums) {
+    count[n] = (count[n] || 0) + 1
+  }
+
+  for (const c in count) {
+    freq[count[c]].push(parseInt(c))
+  }
+  const res = []
+  for (let i = freq.length - 1; i > 0; i--) {
+    for (const n of freq[i]) {
+      res.push(n)
+      if (res.length === k) {
+        return res
+      }
+    }
+  }
+}
+
+module.exports = topKFrequentElements
